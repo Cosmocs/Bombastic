1a4b0438279a84ec7f8bcc9084d5b48b54ee6ce8
Jiuya Lin <jl11935@nyu.edu>
2022-12-01 22:36:31 -0500

    Fixing camera following with animation

--- Bombastic/Assets/cameraFollow.cs
+++ Bombastic/Assets/cameraFollow.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Try to make invisible animation instead of code.
+
+
 public class cameraFollow : MonoBehaviour
 {
     //Attach this script to your camera
@@ -13,19 +17,22 @@ public class cameraFollow : MonoBehaviour
     private Vector3 offset; //so the camera is not directly onto the player
 
     //drag the player object from Hiearchy window to the box in the inspector
-    public GameObject player; //to access the player object. Here it is the red circle.
+    public GameObject player;
+
     //drag the camera object from Hiearychy window to the box in the inspector
     public Camera camera;
     private float halfHeight;
-    private float halfWidth;
-
+    private float moveWidth;
+    private float moveHeight;
+    public float movementDivider;
 
     // Start is called before the first frame update
     void Start()
     {
         //to get the half height and half width of the camera
         halfHeight = camera.orthographicSize;
-        halfWidth = camera.aspect * halfHeight;
+        moveWidth = camera.aspect * halfHeight / movementDivider;
+        moveHeight = halfHeight * 2f / movementDivider;
         //maintain the relative offset
         offset = transform.position - player.transform.position; 
         previousPan = transform.position; //initially the previousPan = original position
@@ -39,7 +46,10 @@ public class cameraFollow : MonoBehaviour
         //check if the player has moved the half of the camera width
         //if yes, change the PreviousPan variable to the new location.
         //if not, make the camera stay. 
-        if (Mathf.Abs(previousPan.x - player.transform.position.x) > halfWidth)
+        if (Mathf.Abs(previousPan.x - player.transform.position.x) > moveWidth)
+        {
+            previousPan = transform.position;
+        }else if (Mathf.Abs(previousPan.y - player.transform.position.y) > moveHeight)
         {
             previousPan = transform.position;
         }
